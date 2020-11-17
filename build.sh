a3f23402eca3abc42c9f6e938c82078a176bee1f
Matthew Paletta <mattpaletta@gmail.com>
2020-11-17 17:29:44 +0000

    Updated build scripts and moved to new smaller image

--- build.sh
+++ build.sh
@@ -1,17 +1,26 @@
 #!/bin/env bash
-# exit when any command fails
-set -e
+set -eou pipefail
 
 command -v docker >/dev/null 2>&1 || { echo >&2 "I require docker but it's not installed.  Aborting."; exit 1; }
 
-docker image build -t cpp-build -f Build .
-docker tag cpp-build mattpaletta/cpp-build
-docker tag cpp-build mattpaletta/cpp-build:latest
-docker push mattpaletta/cpp-build
-docker push mattpaletta/cpp-build:latest
+build_image() {
+	img_name=$1
+	dockerfile=$2
 
-docker image build -t cpp-dev -f Dev .
-docker tag cpp-dev mattpaletta/cpp-dev
-docker tag cpp-dev mattpaletta/cpp-dev:latest
-docker push mattpaletta/cpp-dev 
-docker push mattpaletta/cpp-dev:latest
+	if [[ -e ${dockerfile} ]]; then
+		echo "Found DockerFile"
+	else
+		echo "Failed to find Dockerfile: ${dockerfile}"
+		exit 1
+	fi
+
+	docker image build -t ${img_name} -f ${dockerfile} .
+	docker tag cpp-build mattpaletta/${img_name}
+	docker tag cpp-build mattpaletta/${img_name}:latest
+	docker push mattpaletta/${img_name}
+	docker push mattpaletta/${img_name}:latest
+}
+
+build_image cpp-build Build
+build_image cpp-dev Dev
+build_image cpp-server BuildServer
